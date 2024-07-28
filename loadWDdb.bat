de67f13077ef0992e6f5124d1c4f1d135aacd0b9
Kouji Kozaki <kozaki@osakac.ac.jp>
2024-07-28 09:49:42 +0900

    add some settings

--- loadWDdb.bat
+++ loadWDdb.bat
@@ -1,7 +1,6 @@
 set START=%time%
 
-java -cp WD-HierarchyTool.jar jp.ac.osakac.wdht.HugeGraphFileDBRegister settings/loadWDdb_settings_test-en.txt
-
+java -cp WD-HierarchyTool.jar jp.ac.osakac.wdht.HugeGraphFileDBRegister settings/loadWDdb_settings_all.txt
 echo START %START%
 echo END %time%
 pause
